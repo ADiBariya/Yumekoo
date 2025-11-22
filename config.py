7ed17bf137f8eb0d68ea14ee3e14232926a6bbe7
ADiBariya <adiiibariya@gmail.com>
2025-11-22 14:38:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 class config:
     
     #Client
-    API_ID = 0
-    API_HASH = "0"
-    BOT_TOKEN = "0:0-0"
-    BOT_NAME = "Your_Bot_Name"
-    BOT_USERNAME = "Your_Bot_Username"
+    API_ID = 22733269
+    API_HASH = "d1d8331e5b288c572e8bb6baa7d8f833"
+    BOT_TOKEN = "6385138741:AAHQoO8MZ9N7olgCdCBhj2RxDRDOYc_gCoE"
+    BOT_NAME = "NAhida"
+    BOT_USERNAME = "Nahidax_Bot"
     BOT_ID = 0
     WORKERS = 30
     MAX_MESSAGE_CACHE_SIZE = 100
@@ -13,19 +13,19 @@ class config:
     
      
     #Git
-    GIT_USERNAME = "Your_Git_Username"
-    GIT_URL_WITH_TOKEN = "https://Your_Git_Token@github.com/Your_Git_Username/Yumeko.git" 
+    # GIT_USERNAME = "Your_Git_Username"
+    # GIT_URL_WITH_TOKEN = "https://Your_Git_Token@github.com/Your_Git_Username/Yumeko.git" 
 
 
     #Info
     BOT_VERSION = "x"
-    OWNER_ID = 5630057244
+    OWNER_ID = 1432702628
     OWNER_USERNAME = "senpaiii10"
     SUPPORT_CHAT = -1002069412308
-    SUPPORT_CHAT_USERNAME = "Domihoes"
-    SUPPORT_CHAT_LINK = "https://t.me/Domihoes"
-    LOG_CHANNEL = -1001625036774
-    ERROR_LOG_CHANNEL = -1001625036774
+    SUPPORT_CHAT_USERNAME = "Radhasprt"
+    SUPPORT_CHAT_LINK = "https://t.me/Radhasprt"
+    LOG_CHANNEL = -1001987915872
+    ERROR_LOG_CHANNEL = -1001987915872
     DOWNLOAD_LOCATION = "./downloads"
     COMMAND_PREFIXES = ["/" , "!" , "." , "#" , "$" , "%" , "&" , "?"] 
     CMD_STARTERS = "/.!&#%$"
@@ -36,8 +36,8 @@ class config:
     
     
     #Database
-    MONGODB_URI = "mongodb://localhost:27017" # Use Local Hosted Or Atlas !!
-    DATABASE_NAME = "Frieren"
+    MONGODB_URI = "mongodb+srv://tanog80742_db_user:vvvqLPgNS3tHkhss@cluster0.mfpx8ib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # Use Local Hosted Or Atlas !!
+    DATABASE_NAME = "Annon"
 
     #API
     ARQ_API_KEY = "RLWCED-WZASYO-AWOLTB-ITBWTP-ARQ"
